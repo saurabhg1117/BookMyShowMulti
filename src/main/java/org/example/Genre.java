577e6aafa4824f8277b60e618485ec8f5c61cf26
saurabhg1117 <saurabhg1117@gmail.com>
2023-07-28 19:45:26 +0530

    Project Book My Show

---
+++ src/main/java/org/example/Genre.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public enum Genre {
+    ACTION, ROMANCE, COMEDY, HORROR;
+}
---
+++ src/main/java/org/example/GuestUser.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class GuestUser extends User {
+    public GuestUser(String name) {
+        super(name);
+    }
+
+    public void register(String username, String password){
+
+    }
+}
